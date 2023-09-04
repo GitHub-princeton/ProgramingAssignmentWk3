42e281e95c60b797af56a1d6faa919326e0c9af0
GitHub-princeton <137022718+GitHub-princeton@users.noreply.github.com>
2023-09-04 12:27:05 -0400

    Update cachematrix.R
    Added comments or explanation. The functions were not changed

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,9 @@
+##creates a special “matrix”, which is really a list containing a function to:
+##set   set matrix
+##get   set matrix
+##setInv  set inverse
+##getInv  set inverse
+
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -11,6 +17,9 @@ makeCacheMatrix <- function(x = matrix()) {
        get = get,
        setInv = setInv,
        getInv = getInv)}
+
+##This function computes the inverse of the matrix returned by makeCacheMatrix above. 
+## If the inverse  already been calculated, then it will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
   inv <- x$getInv()
